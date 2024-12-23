55f5e779f916395d600c07aa85998e30582feb41
RasmusBayThomsen <mikemocals@gmail.com>
2024-12-23 13:00:06 +0100

    Changed base stats

--- Items/Melee/SwiftBrand.cs
+++ Items/Melee/SwiftBrand.cs
@@ -22,15 +22,15 @@ namespace EBF.Items.Melee
             Item.width = 64;//Width of the hitbox of the item (usually the item's sprite width)
             Item.height = 64;//Height of the hitbox of the item (usually the item's sprite height)
 
-            Item.damage = 25;//Item's base damage value
+            Item.damage = 12;//Item's base damage value
             Item.knockBack = 3f;//Float, the item's knockback value. How far the enemy is launched when hit
             Item.DamageType = DamageClass.Melee;//Item's damage type, Melee, Ranged, Magic and Summon. Custom damage are also a thing
             Item.useStyle = ItemUseStyleID.Swing;//The animation of the item when used
-            Item.useTime = 25;//How fast the item is used
-            Item.useAnimation = 25;//How long the animation lasts. For swords it should stay the same as UseTime
+            Item.useTime = 18;//How fast the item is used
+            Item.useAnimation = 18;//How long the animation lasts. For swords it should stay the same as UseTime
             
-            Item.value = Item.sellPrice(copper: 0, silver: 50, gold: 0, platinum: 0);//Item's value when sold
-            Item.rare = ItemRarityID.Cyan;//Item's name colour, this is hardcoded by the modder and should be based on progression
+            Item.value = Item.sellPrice(copper: 40, silver: 5, gold: 1, platinum: 0);//Item's value when sold
+            Item.rare = ItemRarityID.Blue;//Item's name colour, this is hardcoded by the modder and should be based on progression
             Item.UseSound = SoundID.Item1;//The item's sound when it's used
             Item.autoReuse = true;//Boolean, if the item auto reuses if the use button is held
             Item.useTurn = true;//Boolean, if the player's direction can change while using the item
