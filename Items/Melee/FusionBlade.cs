ad64e5af11df75e2ca8ef1f3235276dfedcadd79
RasmusBayThomsen <mikemocals@gmail.com>
2024-12-29 18:11:01 +0100

    Updated fusion blade
    Simplified some code (not fully, aweguy gotta take care of some of it), adjusted stats, added recipe

--- Items/Melee/FusionBlade.cs
+++ Items/Melee/FusionBlade.cs
@@ -11,113 +11,119 @@ using Terraria.ModLoader;
 
 namespace EBF.Items.Melee
 {
-	public class FusionBlade : ModItem
-	{
-		public override void SetStaticDefaults()
-		{
-			base.DisplayName.WithFormatArgs("Fusion Blade");//Name of the Item
-			base.Tooltip.WithFormatArgs("Modeled after the weapons used by the MILITIA branch.\nShoots a big bullet.");//Tooltip of the item
-		}
+    public class FusionBlade : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            base.DisplayName.WithFormatArgs("Fusion Blade");//Name of the Item
+            base.Tooltip.WithFormatArgs("Modeled after the weapons used by the MILITIA branch.\nShoots a big bullet.");//Tooltip of the item
+        }
 
-		public override void SetDefaults()
-		{
-			Item.width = 64;//Width of the hitbox of the item (usually the item's sprite width)
-			Item.height = 64;//Height of the hitbox of the item (usually the item's sprite height)
+        public override void SetDefaults()
+        {
+            Item.width = 64;//Width of the hitbox of the item (usually the item's sprite width)
+            Item.height = 64;//Height of the hitbox of the item (usually the item's sprite height)
 
-			Item.damage = 25;//Item's base damage value
-			Item.knockBack = 1;//Float, the item's knockback value. How far the enemy is launched when hit
-			Item.DamageType = DamageClass.Melee;//Item's damage type, Melee, Ranged, Magic and Summon. Custom damage are also a thing
-			Item.useStyle = ItemUseStyleID.Swing;//The animation of the item when used
-			Item.useTime = 20;//How fast the item is used
-			Item.useAnimation = 20;//How long the animation lasts. For swords it should stay the same as UseTime
+            Item.damage = 45;//Item's base damage value
+            Item.knockBack = 1;//Float, the item's knockback value. How far the enemy is launched when hit
+            Item.DamageType = DamageClass.Melee;//Item's damage type, Melee, Ranged, Magic and Summon. Custom damage are also a thing
+            Item.useStyle = ItemUseStyleID.Swing;//The animation of the item when used
+            Item.useTime = 20;//How fast the item is used
+            Item.useAnimation = 20;//How long the animation lasts. For swords it should stay the same as UseTime
 
-			Item.value = Item.sellPrice(copper: 0, silver: 0, gold: 0, platinum: 0);//Item's value when sold
-			Item.rare = ItemRarityID.Orange;//Item's name colour, this is hardcoded by the modder and should be based on progression
-			Item.UseSound = SoundID.Item1;//The item's sound when it's used
-			Item.autoReuse = true;//Boolean, if the item auto reuses if the use button is held
-			Item.useTurn = false;//Boolean, if the player's direction can change while using the item
+            Item.value = Item.sellPrice(copper: 0, silver: 20, gold: 10, platinum: 0);//Item's value when sold
+            Item.rare = ItemRarityID.Pink;//Item's name colour, this is hardcoded by the modder and should be based on progression
+            Item.UseSound = SoundID.Item1;//The item's sound when it's used
+            Item.autoReuse = true;//Boolean, if the item auto reuses if the use button is held
+            Item.useTurn = false;//Boolean, if the player's direction can change while using the item
 
-			Item.shoot = ModContent.ProjectileType<FusionBlade_BulletBob>();
-			Item.shootSpeed = 1f;
-		}
+            Item.shoot = ModContent.ProjectileType<FusionBlade_BulletBob>();
+            Item.shootSpeed = 1f;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe(amount: 1)
+                .AddIngredient(ItemID.HallowedBar, stack: 12)
+                .AddIngredient(ItemID.Wire, stack: 40)
+                .AddIngredient(ItemID.SoulofFright, stack: 10)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
+        }
     }
 
-	public class FusionBlade_BulletBob : ModProjectile
-	{
-
+    public class FusionBlade_BulletBob : ModProjectile
+    {
         private NPC target;
         private float speed = 15;
-        private bool runOnce = true;
         private float direction;
+        private const int waitingFrames = 30;
 
         public override void SetStaticDefaults()
-		{
-			Main.projFrames[Projectile.type] = 3;
-		}
+        {
+            Main.projFrames[Projectile.type] = 3;
+        }
 
-		public override void SetDefaults()
-		{
-			Projectile.width = 12;
-			Projectile.height = 12;
+        public override void SetDefaults()
+        {
+            Projectile.width = 12;
+            Projectile.height = 12;
 
-			Projectile.aiStyle = 0;
-			Projectile.friendly = true;
-			Projectile.penetrate = 100;
-			Projectile.DamageType = DamageClass.Melee;
+            Projectile.aiStyle = 0;
+            Projectile.friendly = true;
+            Projectile.penetrate = 100;
+            Projectile.DamageType = DamageClass.Melee;
 
-			Projectile.timeLeft = 60 * 5;
+            Projectile.timeLeft = 60 * 5;
 
-			Projectile.localNPCHitCooldown = -1;
-			Projectile.usesLocalNPCImmunity = true;
-		}
+            Projectile.localNPCHitCooldown = -1;
+            Projectile.usesLocalNPCImmunity = true;
+        }
 
-		public override void OnKill(int timeLeft)
-		{
-			Collision.HitTiles(Projectile.position + Projectile.velocity, Projectile.velocity, Projectile.width, Projectile.height);
-		}
+        public override void OnKill(int timeLeft)
+        {
+            Collision.HitTiles(Projectile.position + Projectile.velocity, Projectile.velocity, Projectile.width, Projectile.height);
+        }
 
-		public override bool PreAI()
-		{
-            if (runOnce)
-            {
-                direction = Projectile.velocity.ToRotation();
-                Projectile.rotation = direction + (MathF.PI / 2);
-                Projectile.spriteDirection = Projectile.direction;
-                runOnce = false;
-            }
+        public override void OnSpawn(IEntitySource source)
+        {
+            direction = Projectile.velocity.ToRotation();
+            Projectile.rotation = direction + (MathF.PI / 2);
+            Projectile.spriteDirection = Projectile.direction;
+        }
+
+        public override bool PreAI()
+        {
             Projectile.ai[0]++;
-            if (Projectile.ai[0] == 30)
+            if (Projectile.ai[0] == waitingFrames)
             {
                 Projectile.frame = 1;
                 Projectile.velocity *= 4.3f;
             }
-            else if (Projectile.ai[0] > 30)
+            else if (Projectile.ai[0] > waitingFrames)
             {
-             
-
-                if (++Projectile.frameCounter >= 3)
+                Projectile.frameCounter++;
+                if (Projectile.frameCounter >= 3)
                 {
-
                     direction = Projectile.velocity.ToRotation();
                     Projectile.rotation = direction + (MathF.PI / 2);
-					
-                    
+
                     Projectile.frame++;
                     if (Projectile.frame > 2)
                     {
                         Projectile.frame = 1;
                     }
+
                     //DustAI();
-                    Lighting.AddLight(Projectile.Center, new Vector3(255, 165, 0) / 255f);//Orange lighting coming from the center of the Projectile.
+                    Lighting.AddLight(Projectile.Center, new Vector3(255, 165, 0) / 255f); //Orange lighting coming from the center of the Projectile.
 
                     Player player = Main.player[Projectile.owner];
-                    if (Extensions.ProjectileExtensions.ClosestNPC(ref target, 10000, Projectile.Center))
+                    if (Extensions.ProjectileExtensions.ClosestNPC(ref target, 2000, Projectile.Center))
                     {
                         direction = Extensions.ProjectileExtensions.SlowRotation(direction, (target.Center - Projectile.Center).ToRotation(), 3f);
                     }
                     Projectile.velocity = new Vector2(MathF.Cos(direction) * speed, MathF.Sin(direction) * speed);
                     Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Flare);
-                    //Projectile.rotation = direction + (MathF.PI / 2);
                 }
             }
             else
@@ -125,28 +131,17 @@ namespace EBF.Items.Melee
                 Projectile.frame = 0;
             }
 
-			//  float velRotation = Projectile.velocity.ToRotation();
-			// Projectile.rotation = velRotation + MathHelper.ToRadians(90f);
-			// Projectile.spriteDirection = Projectile.direction;
+            // float velRotation = Projectile.velocity.ToRotation();
+            // Projectile.rotation = velRotation + MathHelper.ToRadians(90f);
+            // Projectile.spriteDirection = Projectile.direction;
 
-			return false;
-          
+            return false;
         }
 
-		/*public override bool PreAI()
-		{
-
-            
-
-            return true;
-		}*/
-
-		private void DustAI()
-		{
-			for (int i = 0; i <= 2; i++)
-			{
-				Dust.NewDust(Projectile.position, Projectile.width / 2, Projectile.height / 2, DustID.Firefly);
-			}
-		}
-	}
+        private void DustAI()
+        {
+            Dust.NewDust(Projectile.position, Projectile.width / 2, Projectile.height / 2, DustID.Firefly);
+            Dust.NewDust(Projectile.position, Projectile.width / 2, Projectile.height / 2, DustID.Firefly);
+        }
+    }
 }
