5f815b41d5269b37790c024df87182e0509f15ce
RasmusBayThomsen <mikemocals@gmail.com>
2024-12-30 15:04:59 +0100

    Updated Balance Badge

--- Items/Accessories/Flairs/BalanceBadge.cs
+++ Items/Accessories/Flairs/BalanceBadge.cs
@@ -15,20 +15,18 @@ namespace EBF.Items.Accessories.Flairs
         public override void SetStaticDefaults()
         {
             base.DisplayName.WithFormatArgs("Balance Badge");
-            base.Tooltip.WithFormatArgs("Represents pure equilibrium and bestows a wealth of boosts.\n5 defense\n5% increase to all damage types\nIncreases maximum health and mana by 10\nIncreases movement and attack speed by 5%\nIncreases max minion slots by 1\nIncreases critical rates by 5%");
+            base.Tooltip.WithFormatArgs("Represents pure equilibrium and bestows a wealth of boosts.\n5 defense\nIncreases maximum health and mana by 10\nIncreases movement, attack speed, damage and crit chance by 5%\nIncreases max minion slots by 1");
         }
-
         public override void SetDefaults()
         {
             Item.width = 20;
             Item.height = 19;
             Item.accessory = true;
             Item.rare = ItemRarityID.LightPurple;
+            Item.defense = 5;
         }
-
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
-            player.statDefense += 5;
             player.GetDamage(DamageClass.Generic) += 0.05f;
             player.moveSpeed += 0.05f;
             player.statLifeMax2 += 10;
@@ -36,6 +34,16 @@ namespace EBF.Items.Accessories.Flairs
             player.GetAttackSpeed(DamageClass.Generic) += 0.05f;
             player.GetCritChance(DamageClass.Generic) += 5;
             player.maxMinions += 1;
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe(amount: 1)
+                .AddIngredient<SwordMedal>(stack: 1)
+                .AddIngredient<ShieldMedal>(stack: 1)
+                .AddIngredient<AngryFaic>(stack: 1)
+                .AddIngredient<GreenCross>(stack: 1)
+                .AddTile(TileID.LunarCraftingStation)
+                .Register();
         }
     }
 }
