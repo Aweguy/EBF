eed0e3f9b393307bc642d8b2444acbe737ce8caf
RasmusBayThomsen <mikemocals@gmail.com>
2024-12-30 14:47:41 +0100

    Updated Angry Faic

--- Items/Accessories/Flairs/AngryFaic.cs
+++ Items/Accessories/Flairs/AngryFaic.cs
@@ -14,25 +14,28 @@ namespace EBF.Items.Accessories.Flairs
         public override void SetStaticDefaults()
         {
             base.DisplayName.WithFormatArgs("Angry Faic");
-            base.Tooltip.WithFormatArgs("Wearing this makes you so angry, you want something to be BLAMMED!\n2 defense\nIncreases critical chance by 8% for all types of damage.\nIncreases damage by 8%.\nIncreases enemy aggression.");
+            base.Tooltip.WithFormatArgs("Wearing this makes you so angry, you want something to be BLAMMED!\n2 defense\nIncreases critical chance by 8%.\nIncreases enemy aggression.");
         }
-
         public override void SetDefaults()
         {
             Item.width = 32;
             Item.height = 32;
-            Item.rare = ItemRarityID.LightPurple;
+            Item.rare = ItemRarityID.LightRed;
             Item.accessory = true;
             Item.defense = 2;
         }
-
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
-            
-
-            player.GetCritChance(DamageClass.Generic) += 8;
-            player.GetDamage(DamageClass.Generic) += 0.08f;
             player.aggro += 450;
+            player.GetCritChance(DamageClass.Generic) += 8;
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe(amount: 1)
+                .AddIngredient<TargetBadge>(stack: 1)
+                .AddIngredient(ItemID.AdamantiteBar, stack: 10)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
         }
     }
 }
