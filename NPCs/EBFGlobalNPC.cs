283e94638824b47c936412b62dadb7bcf2e0900a
RasmusBayThomsen <mikemocals@gmail.com>
2024-12-30 16:58:54 +0100

    Added love blade to princess NPC shop

---
+++ NPCs/EBFGlobalNPC.cs
@@ -0,0 +1,19 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using EBF.Items.Melee;
+
+namespace EBF.NPCs
+{
+    internal class EBFGlobalNPC : GlobalNPC
+    {
+        public override void ModifyShop(NPCShop shop)
+        {
+            int type = shop.NpcType;
+
+            if (type == NPCID.Princess)
+            {
+                shop.Add<LoveBlade>();
+            }
+        }
+    }
+}
