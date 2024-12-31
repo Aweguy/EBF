56f48426a92c082d676ac05547b4431d3f858e1f
RasmusBayThomsen <mikemocals@gmail.com>
2024-12-31 15:52:00 +0100

    Projectile: naming conventions and member order

--- Items/Magic/DarkTooth.cs
+++ Items/Magic/DarkTooth.cs
@@ -76,21 +76,20 @@ namespace EBF.Items.Magic
     public class DarkTooth_BlackHole : ModProjectile
     {
         private List<Dust> effectDusts = new List<Dust>(); // create a list of dust object
+
+        private const float dustBoost = 2f; //The offset from the center from which the dust will spawn
+        private const int dustSpawnRate = 100; //bigger = more dust, total dust is dustSpawnTime * dustSpawnRate
+        private const float defaultSuckRange = 160;//The default range in which objects will be SUCCED
         private float dustSpeed = 5f; //how fast the dust moves
-        private float dustBoost = 2f; //The offset from the center from which the dust will spawn
-        private int dustSpawnRate = 100; //bigger = more dust, total dust is dustSpawnTime*dustSpawnRate
         private float gravMagnitude; //The power of the gravitational force
-        private static float DefaultSuckRange = 160;//The default range in which objects will be SUCCED
-        private float SuckRange = 160;//The current range in which objects will be SUCCED
+        private float suckRange = 160;//The current range in which objects will be SUCCED
+        private int baseWidth;
+        private int baseHeight;
 
         public override void SetStaticDefaults()
         {
             Main.projFrames[Projectile.type] = 10;
         }
-
-        private int baseWidth;
-        private int baseHeight;
-
         public override void SetDefaults()
         {
             Projectile.width = 128;
@@ -107,7 +106,6 @@ namespace EBF.Items.Magic
             baseWidth = Projectile.width;
             baseHeight = Projectile.height;
         }
-
         public override bool PreAI()
         {
             Player player = Main.player[Projectile.owner];
@@ -164,21 +162,21 @@ namespace EBF.Items.Magic
                         // If the player channels the weapon, do something. This check only works if item.channel is true for the weapon.
                         if (player.channel)
                         {
-                            SuckRange = DefaultSuckRange * Projectile.scale;
-                            if (SuckRange > 1600f)
+                            suckRange = defaultSuckRange * Projectile.scale;
+                            if (suckRange > 1600f)
                             {
-                                SuckRange = 1600f;
+                                suckRange = 1600f;
                             }
 
                             Scaling(player, oldSize);//The growth of the black hole
 
-                            Sucking(SuckRange);//Sucking enemy npcs (or friendly) not bosses
+                            NPCSucking(suckRange);//Sucking enemy npcs (or friendly) not bosses
 
                             //PlayerSucking(player, SuckRange);//Sucking the player and damaging them if too close/ Was a test
 
-                            GoreSucking(SuckRange);//Sucking gore
+                            GoreSucking(suckRange);//Sucking gore
 
-                            DustSucking(SuckRange);//Sucking dust
+                            DustSucking(suckRange);//Sucking dust
                         }
                         // If the player stops channeling, do something else.
                         else if (Projectile.ai[0] == 0f)//The damage when it ends
@@ -196,183 +194,7 @@ namespace EBF.Items.Magic
             }
             return false;
         }
-
-        //Movement of the black hole
-        private void Movement(Player player)
-        {
-            float maxDistance = 3f; // This also sets the maximun speed the Projectile can reach while following the cursor.
-            Vector2 vectorToCursor = Main.MouseWorld - Projectile.Center;
-            float distanceToCursor = vectorToCursor.Length();
-
-            // Here we can see that the speed of the Projectile depends on the distance to the cursor.
-            if (distanceToCursor > maxDistance)
-            {
-                distanceToCursor = maxDistance / distanceToCursor;
-                vectorToCursor *= distanceToCursor;
-            }
-
-            int velocityXBy1000 = (int)(vectorToCursor.X * 3f);
-            int oldVelocityXBy1000 = (int)(Projectile.velocity.X * 3f);
-            int velocityYBy1000 = (int)(vectorToCursor.Y * 3f);
-            int oldVelocityYBy1000 = (int)(Projectile.velocity.Y * 3f);
-
-            // This code checks if the precious velocity of the Projectile is different enough from its new velocity, and if it is, syncs it with the server and the other clients in MP.
-            // We previously multiplied the speed by 1000, then casted it to int, this is to reduce its precision and prevent the speed from being synced too much.
-            if (velocityXBy1000 != oldVelocityXBy1000 || velocityYBy1000 != oldVelocityYBy1000)
-            {
-                Projectile.netUpdate = true;
-            }
-
-            Projectile.velocity = vectorToCursor;
-        }
-        //The growth rate of the black hole
-        private void Scaling(Player player, Vector2 oldSize)
-        {
-            float change = 0;
-
-            if (Projectile.width <= 150)
-            {
-                change = 0.1f;
-            }
-            else if (Projectile.width <= 325 && Projectile.width > 150)
-            {
-                change = 0.05f;
-            }
-            else if (Projectile.width > 325 && Projectile.width <= 700)
-            {
-                change = 0.025f;
-            }
-
-            Projectile.scale += change; //multiplied by haste if you want to
-
-            Projectile.width = (int)(baseWidth * Projectile.scale);
-            Projectile.height = (int)(baseHeight * Projectile.scale);
-            Projectile.position = Projectile.position - (Projectile.Size - oldSize) / 2f;
-        }
-        //NPC sucking
-        private void Sucking(float SuckingRange)
-        {
-            for (int i = 0; i < Main.maxNPCs; i++)
-            {
-                NPC npc = Main.npc[i];
-                if (npc.active)
-                {
-                    float between = Vector2.Distance(Projectile.Center, npc.Center);//Calculating the distance
-
-                    if (between < 0.1f)
-                    {
-                        between = 0.1f;
-                    }
-                    gravMagnitude = (Projectile.scale * 75) / between; //gravitaional pull equation
-
-                    if (!npc.boss && between <= SuckingRange)
-                    {
-                        npc.velocity += npc.DirectionTo(Projectile.Center) * gravMagnitude;//applying the gravitational pull force calculated above
-                    }
-                }
-            }
-        }
-        //Player Sucking Was just a test, not practical
-        /*private void PlayerSucking(Player player, float SuckingRange)
-        {
-            if (player.active)
-            {
-                float between = Vector2.Distance(Projectile.Center, player.Center);//Distance
-
-                if (between < 0.1f)
-                {
-                    between = 0.1f;
-                }
-                if (between <= 50f * Projectile.scale)
-                {
-                    player.Hurt(PlayerDeathReason.ByCustomReason($"{player.name} was turned into particles!"), (int)(12 * Projectile.scale), 0, true, false);// Damaging the player if too close to the black hole
-                }
-                if (between <= SuckingRange)
-                {
-                    gravMagnitude = (Projectile.scale * 14) / between; //gravitaional pull equation
-                    player.velocity += player.DirectionTo(Projectile.Center) * gravMagnitude;//Final calculation
-                }
-            }
-        }*/
-
-        //Gore Sucking
-        private void GoreSucking(float SuckingRange)
-        {
-            for (int i = 0; i < Main.maxGore; i++)
-            {
-                Gore gore = Main.gore[i];
-                if (gore.active)
-                {
-                    float between = Vector2.Distance(Projectile.Center, gore.position);
-
-                    if (between < 0.1f)
-                    {
-                        between = 0.1f;
-                    }
-                    if (between <= SuckingRange)
-                    {
-                        gravMagnitude = (Projectile.scale * 100) / between; //gravitaional pull equation
-                        gore.velocity -= Vector2.Normalize(gore.position - Projectile.Center) * gravMagnitude;//Final Calculation
-                    }
-                }
-            }
-        }
-
-        //Dust Sucking
-        private void DustSucking(float SuckingRange)
-        {
-            for (int i = 0; i < Main.maxDust; i++)
-            {
-                Dust dust = Main.dust[i];
-                if (dust.active)
-                {
-                    float between = Vector2.Distance(Projectile.Center, dust.position);
-
-                    if (between < 0.1f)
-                    {
-                        between = 0.1f;
-                    }
-                    if (between <= SuckingRange)
-                    {
-                        gravMagnitude = (Projectile.scale * 100) / between; //gravitaional pull equation
-                        dust.velocity -= Vector2.Normalize(dust.position - Projectile.Center) * gravMagnitude;//Final Calculation
-                    }
-                }
-            }
-        }
-        //Damage after it blows up
-        private void Damage()
-        {
-            Projectile.tileCollide = false;
-            // Set to transparent. This Projectile technically lives as  transparent for about 3 frames
-            // change the hitbox size, centered about the original Projectile center. This makes the Projectile damage enemies during the explosion.
-            Projectile.position = Projectile.Center;
-            //Projectile.position.X = Projectile.position.X + (float)(Projectile.width / 2);
-            //Projectile.position.Y = Projectile.position.Y + (float)(Projectile.height / 2);
-            if (Projectile.width <= 150)
-            {
-                Projectile.width += 80;
-                Projectile.height += 80;
-                Projectile.damage = (80 + Projectile.width) * 3;
-            }
-            else if (Projectile.width <= 325 && Projectile.width > 150)
-            {
-                Projectile.width += 220;
-                Projectile.height += 220;
-                Projectile.damage = (220 + Projectile.width) * 4;
-            }
-            else
-            {
-                Projectile.width += 500;
-                Projectile.height += 500;
-                Projectile.damage = (700 + Projectile.width) * 5;
-            }
-            Projectile.Center = Projectile.position;
-            //Projectile.position.X = Projectile.position.X - (float)(Projectile.width / 2);
-            //Projectile.position.Y = Projectile.position.Y - (float)(Projectile.height / 2);
-        }
-        //The dust when the Projectile dies
-        public override void OnKill(int timeLeft)
+        public override void OnKill(int timeLeft) //The dust when the Projectile dies
         {
 
             if (Projectile.width <= 150)
@@ -418,8 +240,7 @@ namespace EBF.Items.Magic
             Projectile.position.X = Projectile.position.X - (float)(Projectile.width / 2);
             Projectile.position.Y = Projectile.position.Y - (float)(Projectile.height / 2);
         }
-        //Code for making thte Projectile animate while its position is centered
-        public override bool PreDraw(ref Color lightColor)
+        public override bool PreDraw(ref Color lightColor) //Code for making thte Projectile animate while its position is centered
         {
             Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
 
@@ -439,10 +260,153 @@ namespace EBF.Items.Magic
             // Do not allow vanilla drawing code to execute.
             return (false);
         }
-
         public override void DrawBehind(int index, List<int> behindNPCsAndTiles, List<int> behindNPCs, List<int> behindProjectiles, List<int> overPlayers, List<int> overWiresUI)
         {
             behindNPCs.Add(index);
+        }
+        private void Movement(Player player) //Movement of the black hole
+        {
+            float maxDistance = 3f; // This also sets the maximun speed the Projectile can reach while following the cursor.
+            Vector2 vectorToCursor = Main.MouseWorld - Projectile.Center;
+            float distanceToCursor = vectorToCursor.Length();
+
+            // Here we can see that the speed of the Projectile depends on the distance to the cursor.
+            if (distanceToCursor > maxDistance)
+            {
+                distanceToCursor = maxDistance / distanceToCursor;
+                vectorToCursor *= distanceToCursor;
+            }
+
+            int velocityXBy1000 = (int)(vectorToCursor.X * 3f);
+            int oldVelocityXBy1000 = (int)(Projectile.velocity.X * 3f);
+            int velocityYBy1000 = (int)(vectorToCursor.Y * 3f);
+            int oldVelocityYBy1000 = (int)(Projectile.velocity.Y * 3f);
+
+            // This code checks if the precious velocity of the Projectile is different enough from its new velocity, and if it is, syncs it with the server and the other clients in MP.
+            // We previously multiplied the speed by 1000, then casted it to int, this is to reduce its precision and prevent the speed from being synced too much.
+            if (velocityXBy1000 != oldVelocityXBy1000 || velocityYBy1000 != oldVelocityYBy1000)
+            {
+                Projectile.netUpdate = true;
+            }
+
+            Projectile.velocity = vectorToCursor;
+        }
+        private void Scaling(Player player, Vector2 oldSize) //The growth rate of the black hole
+        {
+            float change = 0;
+
+            if (Projectile.width <= 150)
+            {
+                change = 0.1f;
+            }
+            else if (Projectile.width <= 325 && Projectile.width > 150)
+            {
+                change = 0.05f;
+            }
+            else if (Projectile.width > 325 && Projectile.width <= 700)
+            {
+                change = 0.025f;
+            }
+
+            Projectile.scale += change; //multiplied by haste if you want to
+
+            Projectile.width = (int)(baseWidth * Projectile.scale);
+            Projectile.height = (int)(baseHeight * Projectile.scale);
+            Projectile.position = Projectile.position - (Projectile.Size - oldSize) / 2f;
+        }
+        private void NPCSucking(float suckingRange)
+        {
+            for (int i = 0; i < Main.maxNPCs; i++)
+            {
+                NPC npc = Main.npc[i];
+                if (npc.active)
+                {
+                    float between = Vector2.Distance(Projectile.Center, npc.Center);//Calculating the distance
+
+                    if (between < 0.1f)
+                    {
+                        between = 0.1f;
+                    }
+                    gravMagnitude = (Projectile.scale * 75) / between; //gravitaional pull equation
+
+                    if (!npc.boss && between <= suckingRange)
+                    {
+                        npc.velocity += npc.DirectionTo(Projectile.Center) * gravMagnitude;//applying the gravitational pull force calculated above
+                    }
+                }
+            }
+        }
+        private void GoreSucking(float suckingRange)
+        {
+            for (int i = 0; i < Main.maxGore; i++)
+            {
+                Gore gore = Main.gore[i];
+                if (gore.active)
+                {
+                    float between = Vector2.Distance(Projectile.Center, gore.position);
+
+                    if (between < 0.1f)
+                    {
+                        between = 0.1f;
+                    }
+                    if (between <= suckingRange)
+                    {
+                        gravMagnitude = (Projectile.scale * 100) / between; //gravitaional pull equation
+                        gore.velocity -= Vector2.Normalize(gore.position - Projectile.Center) * gravMagnitude;//Final Calculation
+                    }
+                }
+            }
+        }
+        private void DustSucking(float suckingRange)
+        {
+            for (int i = 0; i < Main.maxDust; i++)
+            {
+                Dust dust = Main.dust[i];
+                if (dust.active)
+                {
+                    float between = Vector2.Distance(Projectile.Center, dust.position);
+
+                    if (between < 0.1f)
+                    {
+                        between = 0.1f;
+                    }
+                    if (between <= suckingRange)
+                    {
+                        gravMagnitude = (Projectile.scale * 100) / between; //gravitaional pull equation
+                        dust.velocity -= Vector2.Normalize(dust.position - Projectile.Center) * gravMagnitude;//Final Calculation
+                    }
+                }
+            }
+        }
+        private void Damage() //Damage after it blows up
+        {
+            Projectile.tileCollide = false;
+            // Set to transparent. This Projectile technically lives as  transparent for about 3 frames
+            // change the hitbox size, centered about the original Projectile center. This makes the Projectile damage enemies during the explosion.
+            Projectile.position = Projectile.Center;
+            //Projectile.position.X = Projectile.position.X + (float)(Projectile.width / 2);
+            //Projectile.position.Y = Projectile.position.Y + (float)(Projectile.height / 2);
+            if (Projectile.width <= 150)
+            {
+                Projectile.width += 80;
+                Projectile.height += 80;
+                Projectile.damage = (80 + Projectile.width) * 3;
+            }
+            else if (Projectile.width <= 325 && Projectile.width > 150)
+            {
+                Projectile.width += 220;
+                Projectile.height += 220;
+                Projectile.damage = (220 + Projectile.width) * 4;
+            }
+            else
+            {
+                Projectile.width += 500;
+                Projectile.height += 500;
+                Projectile.damage = (700 + Projectile.width) * 5;
+            }
+            Projectile.Center = Projectile.position;
+            //Projectile.position.X = Projectile.position.X - (float)(Projectile.width / 2);
+            //Projectile.position.Y = Projectile.position.Y - (float)(Projectile.height / 2);
         }
     }
 }
