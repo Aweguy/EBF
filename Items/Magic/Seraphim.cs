03ff05ad2deab0c5e2bd4abbb6b3c64a9cef1e17
RasmusBayThomsen <mikemocals@gmail.com>
2024-12-31 14:41:26 +0100

    Added recipe and adjusted stats

--- Items/Magic/Seraphim.cs
+++ Items/Magic/Seraphim.cs
@@ -24,7 +24,7 @@ namespace EBF.Items.Magic
             Item.width = 90;//Width of the hitbox of the item (usually the item's sprite width)
             Item.height = 90;//Height of the hitbox of the item (usually the item's sprite height)
 
-            Item.damage = 20;//Item's base damage value
+            Item.damage = 52;//Item's base damage value
             Item.knockBack = 1f;//Float, the item's knockback value. How far the enemy is launched when hit
             Item.DamageType = DamageClass.Magic;//Item's damage type, Melee, Ranged, Magic and Summon. Custom damage are also a thing
             Item.useStyle = ItemUseStyleID.Swing;//The animation of the item when used
@@ -32,8 +32,8 @@ namespace EBF.Items.Magic
             Item.useAnimation = 10;//How long the animation lasts. For swords it should stay the same as UseTime
 
             Item.value = Item.sellPrice(copper: 0, silver: 0, gold: 0, platinum: 0);//Item's value when sold
-            Item.rare = ItemRarityID.Purple;//Item's name colour, this is hardcoded by the modder and should be based on progression
-            Item.UseSound = SoundID.Item1;//The item's sound when it's used
+            Item.rare = ItemRarityID.Yellow;//Item's name colour, this is hardcoded by the modder and should be based on progression
+            Item.UseSound = SoundID.Item8;//The item's sound when it's used
             Item.autoReuse = false;//Boolean, if the item auto reuses if the use button is held
             Item.useTurn = true;//Boolean, if the player's direction can change while using the item
 
@@ -53,6 +53,13 @@ namespace EBF.Items.Magic
         {
             //Can't use it if your judgement still exists
             return player.ownedProjectileCounts[ModContent.ProjectileType<Seraphim_Judgement>()] < 1;
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe(amount: 1)
+                .AddIngredient(ItemID.SpectreBar, stack: 26)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
         }
     }
 
