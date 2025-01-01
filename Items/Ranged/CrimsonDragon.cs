413b749ba3f5a4e3fa1788dda3a592b8518eddb6
RasmusBayThomsen <mikemocals@gmail.com>
2025-01-01 15:31:53 +0100

    Updated bow

--- Items/Ranged/CrimsonDragon.cs
+++ Items/Ranged/CrimsonDragon.cs
@@ -1,11 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Terraria;
-using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -15,7 +9,7 @@ namespace EBF.Items.Ranged
     {
         public override void SetStaticDefaults()
         {
-            base.DisplayName.WithFormatArgs("");//Name of the Item
+            base.DisplayName.WithFormatArgs("Crimson Dragon");//Name of the Item
             base.Tooltip.WithFormatArgs("");//Tooltip of the item
         }
 
@@ -24,22 +18,23 @@ namespace EBF.Items.Ranged
             Item.width = 26;//Width of the hitbox of the item (usually the item's sprite width)
             Item.height = 70;//Height of the hitbox of the item (usually the item's sprite height)
 
-            Item.damage = 50;//Item's base damage value
+            Item.damage = 30;//Item's base damage value
             Item.knockBack = 5f;//Float, the item's knockback value. How far the enemy is launched when hit
             Item.DamageType = DamageClass.Ranged;//Item's damage type, Melee, Ranged, Magic and Summon. Custom damage are also a thing
             Item.useStyle = ItemUseStyleID.Shoot;//The animation of the item when used
             Item.useTime = 20;//How fast the item is used
             Item.useAnimation = 20;//How long the animation lasts. For swords it should stay the same as UseTime
 
+            Item.value = Item.sellPrice(copper: 0, silver: 20, gold: 10, platinum: 0);//Item's value when sold
+            Item.rare = ItemRarityID.Pink;//Item's name colour, this is hardcoded by the modder and should be based on progression
+            Item.UseSound = SoundID.DD2_BallistaTowerShot;//The item's sound when it's used
+            Item.autoReuse = true;//Boolean, if the item auto reuses if the use button is held
+            Item.useTurn = false;//Boolean, if the player's direction can change while using the item
+            
             Item.useAmmo = AmmoID.Arrow;
             Item.shoot = ProjectileID.WoodenArrowFriendly;
             Item.shootSpeed = 10f;
 
-            Item.value = Item.sellPrice(copper: 0, silver: 0, gold: 0, platinum: 0);//Item's value when sold
-            Item.rare = ItemRarityID.Blue;//Item's name colour, this is hardcoded by the modder and should be based on progression
-            Item.UseSound = SoundID.DD2_BallistaTowerShot;//The item's sound when it's used
-            Item.autoReuse = true;//Boolean, if the item auto reuses if the use button is held
-            Item.useTurn = false;//Boolean, if the player's direction can change while using the item
         }
 
         public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
@@ -48,6 +43,15 @@ namespace EBF.Items.Ranged
             {
                 type = ModContent.ProjectileType<CrimsonDragon_CrimsonArrow>();
             }
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe(amount: 1)
+                .AddIngredient(ItemID.HellwingBow, stack: 1)
+                .AddIngredient(ItemID.HellstoneBar, stack: 10)
+                .AddIngredient(ItemID.SoulofFright, stack: 20)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
         }
     }
 
