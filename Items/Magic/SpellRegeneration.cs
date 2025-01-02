f64bedf137efab93cdf38eabb60c00ecd6368839
RasmusBayThomsen <mikemocals@gmail.com>
2025-01-02 11:38:17 +0100

    Added recipe to regen spell

--- Items/Magic/SpellRegeneration.cs
+++ Items/Magic/SpellRegeneration.cs
@@ -18,7 +18,6 @@ namespace EBF.Items.Magic
             base.DisplayName.WithFormatArgs("Regeneration Spell");
             base.Tooltip.WithFormatArgs("This spell vastly increases your regeneration.\nCosts a lot of mana and has a big cooldown.");
         }
-
         public override void SetDefaults()
         {
             Item.width = 28;
@@ -26,24 +25,30 @@ namespace EBF.Items.Magic
             Item.useStyle = ItemUseStyleID.HoldUp;
             Item.useTime = 100;
             Item.useAnimation = 10;
-            Item.mana = 100;
-            Item.rare = ItemRarityID.Yellow;
+            Item.mana = 50;
+            Item.rare = ItemRarityID.Green;
             Item.value = Item.sellPrice(copper: 0, silver: 0, gold: 0, platinum: 0);
             Item.useTurn = true;
         }
-
         public override bool CanUseItem(Player player)
         {
             int buff = ModContent.BuffType<CooldownRegen>();
             return !player.HasBuff(buff);
         }
-
         public override bool? UseItem(Player player)
         {
             player.AddBuff(ModContent.BuffType<Regeneration>(), 60 * 5);
             player.AddBuff(ModContent.BuffType<CooldownRegen>(), 60 * 30);
 
             return base.UseItem(player);
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe(amount: 1)
+                .AddIngredient(ItemID.Book, stack: 1)
+                .AddIngredient(ItemID.RegenerationPotion, stack: 5)
+                .AddTile(TileID.Bookcases)
+                .Register();
         }
     }
 }
