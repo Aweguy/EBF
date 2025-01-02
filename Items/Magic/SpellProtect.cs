ea3b3f2cd7d849d4db82b5b1d56b203bd9093985
RasmusBayThomsen <mikemocals@gmail.com>
2025-01-02 11:37:01 +0100

    Added recipe to protect spell

--- Items/Magic/SpellProtect.cs
+++ Items/Magic/SpellProtect.cs
@@ -16,7 +16,7 @@ namespace EBF.Items.Magic
     {
         /*public static readonly SoundStyle ProtectSound = new("EBF/Assets/Sounds/Item/Protect")
         {
-            Volume = 2f,
+            Volume = 1f,
             PitchVariance = 1f
         };*/
 
@@ -33,28 +33,31 @@ namespace EBF.Items.Magic
             Item.useStyle = ItemUseStyleID.HoldUp;
             Item.useTime = 100;
             Item.useAnimation = 10;
-            Item.mana = 5;
+            Item.mana = 20;
             Item.rare = ItemRarityID.Yellow;
             Item.value = Item.sellPrice(copper: 0, silver: 0, gold: 0, platinum: 0);
             Item.useTurn = true;
-            /*if (!Main.dedServ)
-            {
-                Item.UseSound = ProtectSound;
-            }*/
+            //Item.UseSound = ProtectSound;
         }
-
         public override bool CanUseItem(Player player)
         {
             int buff = ModContent.BuffType<CooldownProtect>();
             return !player.HasBuff(buff);
         }
-
         public override bool? UseItem(Player player)
         {
             player.AddBuff(ModContent.BuffType<Protected>(), 60 * 10);
             player.AddBuff(ModContent.BuffType<CooldownProtect>(), 60 * 40);
 
             return base.UseItem(player);
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe(amount: 1)
+                .AddIngredient(ItemID.Book, stack: 1)
+                .AddIngredient(ItemID.IronskinPotion, stack: 5)
+                .AddTile(TileID.Bookcases)
+                .Register();
         }
     }
 }
