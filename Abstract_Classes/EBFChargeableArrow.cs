6dcf4eaa1bada5b9413f72af16bf7690f1b4f04a
TheDigitalZero <mikemocals@gmail.com>
2025-01-13 11:37:44 +0100

    Added getters & setters to ReleaseSound
    So the property is consistent with the other properties

--- Abstract_Classes/EBFChargeableArrow.cs
+++ Abstract_Classes/EBFChargeableArrow.cs
@@ -21,7 +21,8 @@ namespace EBF.Abstract_Classes
         /// The sound that plays once the projectile has been released.
         /// <para>Defaults to Item5 (bow shoot sound).</para>
         /// </summary>
-        protected SoundStyle ReleaseSound = SoundID.Item5;
+        protected SoundStyle ReleaseSound { get; set; } = SoundID.Item5;
+
         /// <summary>
         /// The maximum amount of charge an arrow can have, at which point draw time will stop increasing. Draw time starts at 0 and ticks up by 1 every update while an arrow exists.
         /// <br>If you wish to check if the arrow is fully charged, use the FullyCharged property instead.</br>
