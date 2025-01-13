f97a1047ce00565452b00b4e9cb4890fea381858
TheDigitalZero <mikemocals@gmail.com>
2025-01-13 13:55:20 +0100

    Nerfed fairy bow

--- Items/Ranged/Bows/FairyBow.cs
+++ Items/Ranged/Bows/FairyBow.cs
@@ -14,7 +14,7 @@ namespace EBF.Items.Ranged.Bows
             Item.width = 30;//Width of the hitbox of the item (usually the item's sprite width)
             Item.height = 70;//Height of the hitbox of the item (usually the item's sprite height)
 
-            Item.damage = 9;//Item's base damage value
+            Item.damage = 8;//Item's base damage value
             Item.knockBack = 2.5f;//Float, the item's knockback value. How far the enemy is launched when hit
             Item.DamageType = DamageClass.Ranged;//Item's damage type, Melee, Ranged, Magic and Summon. Custom damage are also a thing
             Item.useStyle = ItemUseStyleID.Shoot;//The animation of the item when used
@@ -47,8 +47,8 @@ namespace EBF.Items.Ranged.Bows
         public override void AddRecipes()
         {
             CreateRecipe(amount: 1)
-                .AddIngredient(ItemID.Wood, stack: 40)
-                .AddIngredient(ItemID.LivingWoodWand, stack: 1)
+                .AddIngredient(ItemID.RichMahogany, stack: 40)
+                .AddIngredient(ItemID.BambooBlock, stack: 10)
                 .AddIngredient(ItemID.Silk, stack: 8)
                 .AddTile(TileID.WorkBenches)
                 .Register();
@@ -70,7 +70,7 @@ namespace EBF.Items.Ranged.Bows
             Projectile.aiStyle = ProjAIStyleID.Arrow;
             Projectile.ignoreWater = true;
 
-            MaximumDrawTime = 50;
+            MaximumDrawTime = 80;
             MinimumDrawTime = 20;
             AutoRelease = true;
 
@@ -87,7 +87,7 @@ namespace EBF.Items.Ranged.Bows
             {
                 for (int i = 0; i < 3; i++)
                 {
-                    Projectile.NewProjectileDirect(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.velocity.RotatedByRandom(0.33d), ProjectileID.Leaf, Projectile.damage / 2, Projectile.knockBack, Projectile.owner);
+                    Projectile.NewProjectileDirect(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.velocity.RotatedByRandom(0.2d), ProjectileID.Leaf, Projectile.damage / 2, Projectile.knockBack, Projectile.owner);
                 }
             }
         }
