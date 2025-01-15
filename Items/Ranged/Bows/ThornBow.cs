2b080fb271d43e0cf439c94f472ee4ad3634975c
TheDigitalZero <mikemocals@gmail.com>
2025-01-15 12:39:07 +0100

    Added thorn bow placeholder

---
+++ Items/Ranged/Bows/ThornBow.cs
@@ -0,0 +1,97 @@
+﻿using EBF.Abstract_Classes;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace EBF.Items.Ranged.Bows
+{
+    public class ThornBow : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Weapons.Ranged.Bows";
+        public override void SetDefaults()
+        {
+            Item.width = 26;//Width of the hitbox of the item (usually the item's sprite width)
+            Item.height = 66;//Height of the hitbox of the item (usually the item's sprite height)
+
+            Item.damage = 31;//Item's base damage value
+            Item.knockBack = 2.5f;//Float, the item's knockback value. How far the enemy is launched when hit
+            Item.DamageType = DamageClass.Ranged;//Item's damage type, Melee, Ranged, Magic and Summon. Custom damage are also a thing
+            Item.useStyle = ItemUseStyleID.Shoot;//The animation of the item when used
+            Item.useTime = 10;//How fast the item is used
+            Item.useAnimation = 10;//How long the animation lasts. For swords it should stay the same as UseTime
+
+            Item.value = Item.sellPrice(copper: 0, silver: 90, gold: 8, platinum: 0);//Item's value when sold
+            Item.rare = ItemRarityID.Pink;//Item's name colour, this is hardcoded by the modder and should be based on progression
+            Item.UseSound = SoundID.Item32;//The item's sound when it's used
+            Item.autoReuse = true;//Boolean, if the item auto reuses if the use button is held
+            Item.useTurn = false;//Boolean, if the player's direction can change while using the item
+
+            Item.useAmmo = AmmoID.Arrow;
+            Item.shoot = ProjectileID.WoodenArrowFriendly;
+            Item.shootSpeed = 8f;
+            Item.channel = true;
+            Item.noMelee = true;
+        }
+        public override bool CanUseItem(Player player)
+        {
+            return player.HasAmmo(player.HeldItem) && !player.noItems && !player.CCed;
+        }
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            if (type == ProjectileID.WoodenArrowFriendly)
+            {
+                type = ModContent.ProjectileType<ThornBow_Arrow>();
+            }
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe(amount: 1)
+                .AddIngredient<TheStinger>(stack: 1)
+                .AddIngredient(ItemID.ChlorophyteBar, stack: 20)
+                .AddIngredient(ItemID.Stinger, stack: 10)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
+        }
+    }
+
+    public class ThornBow_Arrow : EBFChargeableArrow
+    {
+        public override string Texture => $"Terraria/Images/Projectile_{ProjectileID.WoodenArrowFriendly}";
+        public override void SetDefaults()
+        {
+            Projectile.width = 10;
+            Projectile.height = 10;
+
+            Projectile.friendly = false;
+            Projectile.tileCollide = true;
+            Projectile.hide = false;
+            Projectile.DamageType = DamageClass.Ranged;
+            Projectile.aiStyle = ProjAIStyleID.Arrow;
+            Projectile.ignoreWater = true;
+
+            MaximumDrawTime = 10;
+            MinimumDrawTime = 5;
+            AutoRelease = true;
+
+            DamageScale = 1.0f;
+            VelocityScale = 1.33f;
+
+            Projectile.localNPCHitCooldown = -1;
+            Projectile.usesLocalNPCImmunity = true;
+        }
+
+        public override void OnProjectileRelease()
+        {
+            if (FullyCharged)
+            {
+                Projectile.Kill();
+                for (int i = 0; i < 3; i++)
+                {
+                    Projectile.NewProjectileDirect(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.velocity.RotatedByRandom(0.2d), ProjectileID.HornetStinger, Projectile.damage / 2, Projectile.knockBack, Projectile.owner);
+                }
+            }
+        }
+    }
+}
