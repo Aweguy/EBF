2136c9577d9f7f1f33fb1e48cc04cc8ec13fdec1
Aweguy <ireneezineli@gmail.com>
2025-01-15 19:28:29 +0200

    Updated Gaia's Bow rarity from ping to orange

--- Items/Ranged/Bows/GaiasBow.cs
+++ Items/Ranged/Bows/GaiasBow.cs
@@ -25,7 +25,7 @@ namespace EBF.Items.Ranged.Bows
             Item.useAnimation = 30;//How long the animation lasts. For swords it should stay the same as UseTime
 
             Item.value = Item.sellPrice(copper: 0, silver: 0, gold: 10, platinum: 0);//Item's value when sold
-            Item.rare = ItemRarityID.Pink;//Item's name colour, this is hardcoded by the modder and should be based on progression
+            Item.rare = ItemRarityID.Orange;//Item's name colour, this is hardcoded by the modder and should be based on progression
             Item.UseSound = SoundID.Item32;//The item's sound when it's used
             Item.autoReuse = true;//Boolean, if the item auto reuses if the use button is held
             Item.useTurn = false;//Boolean, if the player's direction can change while using the item
