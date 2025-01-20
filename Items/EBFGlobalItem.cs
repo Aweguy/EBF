45076f3b9927672b4c38ee1b82aa165e0871d463
TheDigitalZero <mikemocals@gmail.com>
2025-01-20 19:11:19 +0100

    Added fishing crate loot & NPC drop
    I also modified npc shop code to use switch statement cuz it's faster

---
+++ Items/EBFGlobalItem.cs
@@ -0,0 +1,27 @@
+﻿using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.GameContent.ItemDropRules;
+using EBF.Items.Ranged.Guns;
+
+namespace EBF.Items
+{
+    internal class EBFGlobalItem : GlobalItem
+    {
+        public override void ModifyItemLoot(Item item, ItemLoot itemLoot)
+        {
+            switch (item.type)
+            {
+                #region Fishing Crates
+                case ItemID.IronCrateHard:
+                case ItemID.IronCrate:
+
+                    //SteelShark at 10%
+                    itemLoot.Add(ItemDropRule.Common(ModContent.ItemType<SteelShark>(), 10));
+                    break;
+
+                    #endregion
+            }
+        }
+    }
+}
