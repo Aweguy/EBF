be545208e68e1300601f44d1a6789ff37b34fc83
TheDigitalZero <mikemocals@gmail.com>
2025-01-23 19:17:59 +0100

    Crystal Wing alt-fire effect (ik it looks goofy)

--- Items/Ranged/Guns/CrystalWing.cs
+++ Items/Ranged/Guns/CrystalWing.cs
@@ -3,6 +3,7 @@ using EBF.Buffs;
 using EBF.Buffs.Cooldowns;
 using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -70,6 +71,7 @@ namespace EBF.Items.Ranged.Guns
     }
     public class CrystalWingLauncher : EBFLauncher
     {
+        private const float beamWidth = 0.5f;
         public override string Texture => "EBF/Items/Ranged/Guns/CrystalWing";
         public override void SetDefaults()
         {
@@ -79,19 +81,51 @@ namespace EBF.Items.Ranged.Guns
             Projectile.friendly = false;
             Projectile.DamageType = DamageClass.Ranged;
             Projectile.tileCollide = false;
+
+            ChargeSound = SoundID.Item162;
+            MaxCharge = 120;
         }
-        public override void OnShoot(Vector2 barrelEnd, int type)
+        public override void OnSpawn(IEntitySource source)
         {
+            //Handle shooting sound (has to be done before the OnShoot() method is called)
             Player player = Main.player[Projectile.owner];
             if (player.HasBuff<Charged>())
             {
-                Projectile proj = Projectile.NewProjectileDirect(Projectile.GetSource_FromThis(), barrelEnd, Projectile.velocity, ProjectileID.FrostWave, Projectile.damage, Projectile.knockBack, Projectile.owner);
-                proj.friendly = true;
+                ActiveTime = 60;
+                ShootSound = SoundID.Item163;
+            }
+            else
+            {
+                ShootSound = SoundID.Item4;
+            }
+        }
+        public override void OnShoot(Vector2 barrelEnd, int type)
+        {
+            //Apply or remove buffs
+            Player player = Main.player[Projectile.owner];
+            if (player.HasBuff<Charged>())
+            {
                 player.ClearBuff(ModContent.BuffType<Charged>());
             }
             else
             {
                 player.AddBuff(ModContent.BuffType<Charged>(), 60 * 120);
+            }
+        }
+        public override void WhileShoot(Vector2 barrelEnd, int type)
+        {
+            Player player = Main.player[Projectile.owner];
+            if (!player.HasBuff<Charged>()) //We check false instead of true because WhileShoot() runs after OnShoot(). I know this is a bad way of doing it.
+            {
+                Projectile.frameCounter++;
+                if (Projectile.frameCounter > 1) //Ignore first frame because the buff system probably adds buffs between updates.
+                {
+                    //Randomize 
+                    Vector2 verticalOffset = (Projectile.Center - barrelEnd).RotatedBy(MathHelper.PiOver2) * Main.rand.NextFloatDirection() * beamWidth;
+                    Projectile proj = Projectile.NewProjectileDirect(Projectile.GetSource_FromThis(), barrelEnd + verticalOffset, Projectile.velocity, ProjectileID.LaserMachinegunLaser, Projectile.damage / 4, Projectile.knockBack, Projectile.owner);
+                    proj.friendly = true;
+                    proj.penetrate = -1;
+                }
             }
         }
     }
