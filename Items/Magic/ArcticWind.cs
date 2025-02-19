18cfba5bfe10f0a495dc2799f8b8494923104090
TheDigitalZero <mikemocals@gmail.com>
2025-02-19 22:35:00 +0100

    Added Arctic Wind

---
+++ Items/Magic/ArcticWind.cs
@@ -0,0 +1,95 @@
+
+using EBF.Abstract_Classes;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Audio;
+
+namespace EBF.Items.Magic
+{
+    public class ArcticWind : EBFStaff, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Weapons.Magic";
+
+        public override void SetDefaultsSafe()
+        {
+            Item.width = 40;//Width of the hitbox of the item (usually the item's sprite width)
+            Item.height = 40;//Height of the hitbox of the item (usually the item's sprite height)
+
+            Item.damage = 11;//Item's base damage value
+            Item.knockBack = 4;//Float, the item's knockback value. How far the enemy is launched when hit
+            Item.mana = 6;//The amount of mana this item consumes on use
+
+            Item.useTime = 4;//How fast the item is used
+            Item.useAnimation = 38;//How long the animation lasts. For swords it should stay the same as UseTime
+
+            Item.value = Item.sellPrice(copper: 0, silver: 30, gold: 0, platinum: 0);//Item's value when sold
+            Item.rare = ItemRarityID.Blue;//Item's name colour, this is hardcoded by the modder and should be based on progression
+            Item.autoReuse = true;//Boolean, if the item auto reuses if the use button is held
+            Item.useTurn = false;//Boolean, if the player's direction can change while using the item
+            
+            Item.shoot = ModContent.ProjectileType<ArcticWind_Icecicle>();
+            Item.shootSpeed = 12f;
+        }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            //Quirky way to shoot 3 times
+            if (player.itemAnimation <= 30)
+            {
+                player.itemTime = 30;
+            }
+
+            position = StaffHead + Main.rand.NextVector2Circular(16, 16);
+            velocity = position.DirectionTo(Main.MouseWorld).RotatedByRandom(0.1f) * Item.shootSpeed;
+            
+            //Spawn the projecile
+            SoundEngine.PlaySound(SoundID.Item9, player.Center);
+            Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI, 0f);
+            return false;
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe(amount: 1)
+                .AddIngredient(ItemID.IceBlock, stack: 40)
+                .AddIngredient(ItemID.Sapphire, stack: 10)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+    }
+
+    public class ArcticWind_Icecicle : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 14;
+            Projectile.height = 14;
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Magic;
+            Projectile.tileCollide = true;
+        }
+        public override void OnSpawn(IEntitySource source)
+        {
+            //Spawn dust
+            for (int i = 0; i < 3; i++)
+            {
+                Dust.NewDust(Projectile.Center, 0, 0, DustID.Ice);
+            }
+        }
+        public override void AI()
+        {
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+        }
+        public override void OnKill(int timeLeft)
+        {
+            SoundEngine.PlaySound(SoundID.Item27, Projectile.Center);
+
+            //Spawn dust
+            for (int i = 0; i < 3; i++)
+            {
+                Dust.NewDust(Projectile.Center, 0, 0, DustID.Ice);
+            }
+        }
+    }
+}
