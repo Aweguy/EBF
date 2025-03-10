8557d51cf7582b0b25853b5f02864d5c61382a61
Justin Rush <awesomejcrush@gmail.com>
2025-03-10 18:15:00 -0400

    Adding Gold Star

---
+++ Items/Accessories/Flairs/GoldStar.cs
@@ -0,0 +1,35 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace EBF.Items.Accessories.Flairs
+{
+    public class GoldStar : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Accessories";
+        public override void SetDefaults()
+        {
+            Item.width = 25;
+            Item.height = 25;
+            Item.accessory = true;
+            Item.value = Item.sellPrice(copper: 0, silver: 0, gold: 5, platinum: 0);
+            Item.rare = ItemRarityID.Pink;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetDamage(DamageClass.Magic) += 0.15f;
+            player.GetDamage(DamageClass.Summon) += 0.15f;
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe(amount: 1)
+                .AddIngredient(ItemID.HallowedBar, stack: 15)
+                .AddIngredient(ItemID.SoulofSight, stack: 5)
+                .AddIngredient(ItemID.SoulofMight, stack: 5)
+                .AddIngredient(ItemID.SoulofFright, stack: 5)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
+        }
+    }
+}
