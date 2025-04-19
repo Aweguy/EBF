e7da4a097e40ab1782953d989e6552699db2253b
RasmusBayThomsen <mikemocals@gmail.com>
2025-04-19 11:10:50 +0200

    Lance can now attack

--- NPCs/TownNPCs/Lance.cs
+++ NPCs/TownNPCs/Lance.cs
@@ -1,13 +1,14 @@
 ﻿using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using Terraria.GameContent.Personalities;
 using Terraria.GameContent.Bestiary;
 using Terraria.Localization;
 using EBF.Abstract_Classes;
 using EBF.Items.Ranged.Guns;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Terraria.GameContent;
 
 namespace EBF.NPCs.TownNPCs
 {
@@ -18,13 +19,13 @@ namespace EBF.NPCs.TownNPCs
             Main.npcFrameCount[NPC.type] = 25;
             NPCID.Sets.ExtraFramesCount[NPC.type] = 9;
             NPCID.Sets.AttackFrameCount[NPC.type] = 4;
-            NPCID.Sets.DangerDetectRange[NPC.type] = 16 * 5;
-            NPCID.Sets.AttackType[NPC.type] = 3;
+            NPCID.Sets.DangerDetectRange[NPC.type] = 16 * 30;
+            NPCID.Sets.AttackType[NPC.type] = 1;
             NPCID.Sets.AttackTime[NPC.type] = 30;
             NPCID.Sets.AttackAverageChance[NPC.type] = 30;
             NPCID.Sets.HatOffsetY[NPC.type] = 8;
 
-            //The following line requires an emote bubble asset for Matt
+            //The following line requires an emote bubble asset for Lance
             //NPCID.Sets.FaceEmote[Type] = ModContent.EmoteBubbleType<ExamplePersonEmote>(); // Makes other NPCs talk about this NPC when in the world.
 
             NPC.Happiness
@@ -70,22 +71,31 @@ namespace EBF.NPCs.TownNPCs
         }
         public override void TownNPCAttackStrength(ref int damage, ref float knockback)
         {
-            damage = 70;
-            knockback = 5f;
+            damage = 60;
+            knockback = 4f;
         }
         public override void TownNPCAttackCooldown(ref int cooldown, ref int randExtraCooldown)
         {
             cooldown = 10;
             randExtraCooldown = 10;
         }
-        public override void TownNPCAttackSwing(ref int itemWidth, ref int itemHeight)
+        public override void DrawTownAttackGun(ref Texture2D item, ref Rectangle itemFrame, ref float scale, ref int horizontalHoldoutOffset)
         {
-            itemWidth = itemHeight = 64;
+            var texture = TextureAssets.Projectile[ModContent.ProjectileType<ShadowBlasterSidearm>()].Value;
+            item = texture;
+            itemFrame = texture.Bounds;
+            scale = 0.8f;
+            horizontalHoldoutOffset = -4;
         }
-        public override void DrawTownAttackSwing(ref Texture2D item, ref Rectangle itemFrame, ref int itemSize, ref float scale, ref Vector2 offset)
+        public override void TownNPCAttackProj(ref int projType, ref int attackDelay)
         {
-            Main.GetItemDrawFrame(ItemID.WoodenSword, out item, out itemFrame);
-            itemSize = 16;
+            projType = ModContent.ProjectileType<ShadowBlaster_DarkShot>();
+            attackDelay = 1;
+        }
+        public override void TownNPCAttackProjSpeed(ref float multiplier, ref float gravityCorrection, ref float randomOffset)
+        {
+            multiplier = 8f;
+            randomOffset = 0.1f;
         }
     }
 }
