33e335c0d1f683dab22e0130e8f70e59e87217cf
TheDigitalZero <mikemocals@gmail.com>
2025-04-27 12:11:20 +0200

    cleaned up EBFPlayer.cs

--- EBFPlayer.cs
+++ EBFPlayer.cs
@@ -1,15 +1,4 @@
-﻿using Microsoft.Xna.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Timers;
-using Terraria;
-using Terraria.Chat;
-using Terraria.ID;
-using Terraria.Localization;
+﻿using Terraria;
 using Terraria.ModLoader;
 
 namespace EBF
@@ -17,27 +6,14 @@ namespace EBF
     public class EBFPlayer : ModPlayer
     {
         public bool tetrominoEquipped = false;
-
-        public override void ResetEffects()
-        {
-            base.ResetEffects();
-            tetrominoEquipped = false;
-        }
-
-        public override void ModifyHurt(ref Player.HurtModifiers modifiers)
-        {
-            base.ModifyHurt(ref modifiers);
-            modifiers.ModifyHurtInfo += ModifyHurtInfo;
-        }
-
+        public override void ResetEffects() => tetrominoEquipped = false;
+        public override void ModifyHurt(ref Player.HurtModifiers modifiers) => modifiers.ModifyHurtInfo += ModifyHurtInfo;
         private void ModifyHurtInfo(ref Player.HurtInfo info)
         {
-            if(info.Damage % 10 == 4 && tetrominoEquipped)
+            if(tetrominoEquipped && info.Damage % 10 == 4)
             {
-                //ChatHelper.BroadcastChatMessage(NetworkText.FromLiteral("Damage taken: " + info.Damage), Color.Pink);
                 info.Damage = 4;
             }
         }
-
     }
 }
