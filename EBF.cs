c6107c4ed381a1faf07a367122f8fbaaea691fd7
TheDigitalZero <mikemocals@gmail.com>
2025-04-27 12:09:04 +0200

    Removed obsolete code in EBF.cs

--- EBF.cs
+++ EBF.cs
@@ -3,22 +3,5 @@ using Terraria.ModLoader;
 namespace EBF
 {
 	public class EBF : Mod
-	{
-        private static EBF instance;
-        public static EBF Instance
-        {
-            get 
-            {
-                if (instance == null)
-                {
-                    instance = new EBF();
-                }
-                return instance;
-            }
-        }
-        internal enum EpicMessageType : byte
-        {
-            EpicPlayerSyncPlayer
-        }
-    }
+	{ }
 }
