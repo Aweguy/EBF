7e5843acc387d87341cf44444dfb303a7ac9f10f
TheDigitalZero <mikemocals@gmail.com>
2025-05-21 18:04:10 +0200

    Added base class to turrets

--- NPCs/Machines/LaserTurret.cs
+++ NPCs/Machines/LaserTurret.cs
@@ -12,49 +12,22 @@ using EBF.Extensions;
 
 namespace EBF.NPCs.Machines
 {
-    public class LaserTurret : ModNPC
+    public class LaserTurret : Turret
     {
-        private Asset<Texture2D> baseTexture;
-        private Asset<Texture2D> bodyTexture;
-        private Asset<Texture2D> glowTexture;
-        private Rectangle baseRect;
         private ref float IsLasering => ref NPC.ai[0]; //This value is also read by Neon Valkyrie so it doesn't do BS maneuvers.
         private ref float Timer => ref NPC.localAI[0];
         private ref float AttackChoice => ref NPC.localAI[1];
         private ref float BallsFired => ref NPC.localAI[2];
-        public override void SetStaticDefaults()
-        {
-            NPCID.Sets.DontDoHardmodeScaling[Type] = true;
-        }
-        public override void SetDefaults()
+        public override void SetDefaultsSafe()
         {
             NPC.width = 70;
             NPC.height = 56;
             NPC.damage = 30;
             NPC.defense = 18;
             NPC.lifeMax = 2000;
-            NPC.value = 100;
-            NPC.noGravity = true;
-            NPC.HitSound = SoundID.NPCHit4;
-            NPC.knockBackResist = 0;
-
-            baseTexture = ModContent.Request<Texture2D>("EBF/NPCs/Machines/NV_TurretBase");
-            bodyTexture = ModContent.Request<Texture2D>(Texture);
-            glowTexture = ModContent.Request<Texture2D>(Texture + "_Glow");
         }
-        public override void OnSpawn(IEntitySource source)
+        public override void AISafe()
         {
-            baseRect = new Rectangle(0, 0, baseTexture.Width(), baseTexture.Height() / 2);
-        }
-        public override void AI()
-        {
-            NPC.TargetClosest(false); // only if not shooting. We don't want it to flip while laserbeaming.
-
-            //Flip direction based on rotation
-            NPC.direction = Math.Sign(NPC.rotation.ToRotationVector2().X);
-            if (NPC.direction == 0)
-                NPC.direction = 1;
-
             Player player = Main.player[NPC.target];
             Timer++;
 
@@ -70,7 +43,7 @@ namespace EBF.NPCs.Machines
 
             if (Timer < 240)
             {
-                RotateToTarget(player);
+                LerpRotationToTarget(player, 0.1f);
                 return;
             }
 
@@ -106,42 +79,6 @@ namespace EBF.NPCs.Machines
                 AttackChoice = Main.rand.Next(2);
             }
         }
-        public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
-        {
-            if (!glowTexture.IsLoaded || !bodyTexture.IsLoaded || !baseTexture.IsLoaded)
-                return false;
-
-            // Adjusts the pivot point, so the turret rotates around the attachment and not its center.
-            var funnyOffset = Vector2.UnitX * 12;
-
-            //Draw base back
-            baseRect.Y = 0;
-            var position = NPC.Center + new Vector2(0, (NPC.height - baseRect.Height) / 2) - screenPos;
-            var origin = baseRect.Size() * 0.5f;
-            var flipX = NPC.direction == 1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
-            spriteBatch.Draw(baseTexture.Value, position, baseRect, drawColor, 0f, origin, 1f, flipX, 0);
-
-            //Draw body
-            position = NPC.Center + new Vector2(0, -10) - screenPos - (funnyOffset * NPC.direction);
-            origin = bodyTexture.Size() * 0.5f + funnyOffset;
-            var realRotation = NPC.rotation + MathHelper.Pi;
-            var flipY = NPC.direction == 1 ? SpriteEffects.FlipVertically : SpriteEffects.None;
-            spriteBatch.Draw(bodyTexture.Value, position, null, drawColor, realRotation, origin, 1f, flipY, 0);
-
-            //Draw glow
-            var pulse = (float)Math.Abs(Math.Sin(Main.time * 0.02f));
-            var color = new Color(pulse, pulse, pulse);
-            spriteBatch.Draw(glowTexture.Value, position, null, color, realRotation, origin, 1f, flipY, 0);
-
-            //Draw base front
-            baseRect.Y += baseRect.Height;
-            position = NPC.Center + new Vector2(0, (NPC.height - baseRect.Height) / 2) - screenPos;
-            origin = baseRect.Size() * 0.5f;
-            spriteBatch.Draw(baseTexture.Value, position, baseRect, drawColor, 0f, origin, 1f, flipX, 0);
-
-            return false;
-        }
-
         private void ShootBalls()
         {
             if(Main.GameUpdateCount % 10 == 0)
@@ -163,8 +100,6 @@ namespace EBF.NPCs.Machines
                 }
             }
         }
-
-        #region LaserAI
         private void ShootLaser()
         {
             var velocity = NPC.rotation.ToRotationVector2();
@@ -181,12 +116,6 @@ namespace EBF.NPCs.Machines
             var vel = pos.DirectionTo(NPC.Center);
             var dust = Dust.NewDustPerfect(pos, DustID.AmberBolt, vel, 0, default, 1.25f);
             dust.noGravity = true;
-        }
-        private void RotateToTarget(Player player)
-        {
-            float angleToPlayer = NPC.DirectionTo(player.Center).ToRotation();
-            float angleDiff = MathHelper.WrapAngle(angleToPlayer - NPC.rotation);
-            NPC.rotation += angleDiff * 0.1f;
         }
         private void RotateAheadOfTarget(Player player)
         {
@@ -218,7 +147,6 @@ namespace EBF.NPCs.Machines
 
             NPC.rotation = newAngle;
         }
-        #endregion
     }
     public class LaserTurret_Ball : ModProjectile
     {
