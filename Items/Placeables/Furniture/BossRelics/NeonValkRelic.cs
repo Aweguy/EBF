043ed6f8f86dff063269a65a5202905cdd1445ce
TheDigitalZero <mikemocals@gmail.com>
2025-06-17 14:44:42 +0200

    added a working relic

---
+++ Items/Placeables/Furniture/BossRelics/NeonValkRelic.cs
@@ -0,0 +1,20 @@
+﻿using EBF.Tiles.Furniture.BossRelics;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace EBF.Items.Placeables.Furniture.BossRelics
+{
+    internal class NeonValkRelic : ModItem
+    {
+        public override void SetDefaults()
+        {
+            // The place style (here by default 0) is important if you decide to have more than one relic share the same tile type (more on that in the tiles' code)
+            Item.DefaultToPlaceableTile(ModContent.TileType<NeonValkRelicTile>(), 0);
+
+            Item.width = 30;
+            Item.height = 40;
+            Item.master = true;
+            Item.value = Item.buyPrice(0, 5);
+        }
+    }
+}
