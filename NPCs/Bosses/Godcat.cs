0ad19a56fb71756072aa0d25e140a45120ad676f
RasmusBayThomsen <mikemocals@gmail.com>
2025-06-30 08:52:34 +0200

    added more attacks, and made her attack faster

--- NPCs/Bosses/Godcat.cs
+++ NPCs/Bosses/Godcat.cs
@@ -1,9 +1,11 @@
 ﻿using EBF.Extensions;
 using EBF.Items.Magic;
+using EBF.Items.Melee;
 using EBF.Systems;
 using Microsoft.Xna.Framework;
 using System;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.GameContent.Bestiary;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -109,17 +111,42 @@ namespace EBF.NPCs.Bosses
             }
 
             //Handle attacks
-            if (Main.GameUpdateCount % 360 == 0)
+            if (Main.GameUpdateCount % 200 == 0)
             {
-                switch (Main.rand.Next(2))
+                switch (Main.rand.Next(4))
                 {
                     case 0:
-                        CreateJudgementStream(player);
-                        break;
-                    case 1:
                         CreateJudgementWave(player);
                         break;
+                    case 1:
+                        //Flurry of light blade
+                        NPC.ai[0] = 30;
+                        break;
+                    case 2:
+                        CreateSeikenRing(14, 10);
+                        CreateSeikenRing(8, 6);
+                        CreateSeikenRing(6, 4);
+                        break;
+                    case 3:
+                        //Cast multiple return balls
+                        NPC.ai[1] = 3;
+                        CreateBallArc(player, 0.66f, 6, 9f);
+                        break;
                 }
+            }
+
+            //Handle light blade flurry
+            if (Main.GameUpdateCount % 4 == 0 && NPC.ai[0] > 0)
+            {
+                CreateStormSeiken(player);
+                NPC.ai[0]--;
+            }
+
+            //Handle return balls
+            if (Main.GameUpdateCount % 25 == 0 && NPC.ai[1] > 0)
+            {
+                CreateReturnBall(player, 15);
+                NPC.ai[1]--;
             }
         }
         public override void OnKill()
@@ -156,8 +183,8 @@ namespace EBF.NPCs.Bosses
         }
         private void Move(Player player)
         {
-            var preferredPosition = player.Center + new Vector2(-300 * NPC.direction, -100);
-            NPC.position = Vector2.Lerp(NPC.position, preferredPosition, 0.025f);
+            var preferredPosition = player.Center + new Vector2(-500 * NPC.direction, -100);
+            NPC.position = Vector2.Lerp(NPC.position, preferredPosition, 0.03f);
         }
         private void DodgeOverlappingProjectile()
         {
@@ -184,15 +211,6 @@ namespace EBF.NPCs.Bosses
                 CreateJudgementAt(player.Bottom + new Vector2(-distance, 0), delay);
             }
         }
-        private void CreateJudgementStream(Player player)
-        {
-            var dir = player.velocity.X != 0 ? Math.Sign(player.velocity.X) : NPC.direction; // If player is not moving, use NPC direction
-            dir *= 100;
-            for (int i = 0; i < 5; i++)
-            {
-                CreateJudgementAt(player.Bottom + new Vector2(dir * i, 0), delay: 10 * i);
-            }
-        }
         private void CreateJudgementAt(Vector2 position)
         {
             var type = ModContent.ProjectileType<Seraphim_Judgement>();
@@ -208,6 +226,35 @@ namespace EBF.NPCs.Bosses
             proj.timeLeft = delay;
             proj.friendly = false;
             proj.hostile = true;
+        }
+        private void CreateStormSeiken(Player player)
+        {
+            var position = NPC.Center + Main.rand.NextVector2CircularEdge(64, 64);
+            var velocity = Vector2.Normalize(player.Center - position) * 14f;
+            var type = ModContent.ProjectileType<Godcat_LightBlade>();
+            Projectile.NewProjectile(NPC.GetSource_FromAI(), position, velocity.RotatedByRandom(0.33f), type, NPC.damage, 3f, -1, NPC.target);
+        }
+        private void CreateSeikenRing(int amount, int speed)
+        {
+            for (float theta = 0; theta < MathF.Tau; theta += MathF.Tau / amount)
+            {
+                var type = ModContent.ProjectileType<Godcat_LightBlade>();
+                Projectile.NewProjectile(NPC.GetSource_FromAI(), NPC.Center, Vector2.UnitX.RotatedBy(theta) * speed, type, NPC.damage, 3f, -1, NPC.target);
+            }
+        }
+        private void CreateReturnBall(Player player, float speed)
+        {
+            var type = ModContent.ProjectileType<Godcat_LightReturnBall>();
+            Projectile.NewProjectile(NPC.GetSource_FromAI(), NPC.Center, NPC.DirectionTo(player.Center) * speed, type, NPC.damage, 3f, -1, NPC.whoAmI);
+        }
+        private void CreateBallArc(Player player, float spread, int amount, float speed)
+        {
+            var type = ModContent.ProjectileType<Godcat_LightBall>();
+            for (float theta = -spread; theta < spread; theta += 2 * spread / amount)
+            {
+                var velocity = NPC.DirectionTo(player.Center).RotatedBy(theta) * Main.rand.NextFloat(0.9f, 1.1f) * speed;
+                Projectile.NewProjectile(NPC.GetSource_FromAI(), NPC.Center, velocity, type, NPC.damage, 3f);
+            }
         }
     }
     /// <summary>
@@ -225,6 +272,7 @@ namespace EBF.NPCs.Bosses
             Projectile.width = 0;
             Projectile.height = 0;
             Projectile.timeLeft = 60; // 1 second delay by default
+            Projectile.tileCollide = false;
         }
         public override void AI()
         {
@@ -249,5 +297,199 @@ namespace EBF.NPCs.Bosses
             }
         }
         public override bool PreDraw(ref Color lightColor) => false; // Don't draw this 
+    }
+
+    /// <summary>
+    /// Variation of Light Blade that behaves differently to the one casted by heaven's gate.
+    /// </summary>
+    public class Godcat_LightBlade : ModProjectile
+    {
+        private const float turnSpeed = 0.5f;
+        private bool animate = true;
+        private float speed;
+        private Player Target => Main.player[(int)Projectile.ai[0]];
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[Projectile.type] = 11;
+        }
+        public override void SetDefaults()
+        {
+            Projectile.width = 10;
+            Projectile.height = 10;
+            Projectile.timeLeft = 240;
+            Projectile.hostile = true;
+            Projectile.friendly = false;
+            Projectile.tileCollide = false;
+        }
+        public override string Texture => "EBF/Items/Melee/HeavensGate_LightBlade";
+        public override bool? CanHitNPC(NPC target) => false;
+        public override bool? CanDamage() => !animate;
+        public override bool ShouldUpdatePosition() => !animate;
+        public override void OnSpawn(IEntitySource source)
+        {
+            speed = Projectile.velocity.Length();
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+        }
+        public override void AI()
+        {
+            if (animate)
+            {
+                Animate();
+            }
+            else if (Projectile.timeLeft < 60)
+            {
+                animate = true;
+            }
+            else if (Target != null)
+            {
+                speed *= 1.01f;
+
+                //Slight homing behavior while flying
+                var angleToTarget = Projectile.AngleTo(Target.Center) + MathHelper.PiOver2;
+                Projectile.rotation = ProjectileExtensions.SlowRotation(Projectile.rotation, angleToTarget, turnSpeed);
+                Projectile.velocity = (Projectile.rotation - MathHelper.PiOver2).ToRotationVector2() * speed;
+            }
+
+            //Create dust
+            if (Main.rand.NextBool(3))
+            {
+                var dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.AncientLight);
+                dust.noGravity = true;
+            }
+        }
+        public override void OnKill(int timeLeft)
+        {
+            var velocity = Vector2.Normalize(Projectile.oldVelocity);
+            var position = Projectile.position + velocity * 16f;
+
+            for (int i = 0; i < 20; i++)
+            {
+                Dust dust = Dust.NewDustDirect(position, Projectile.width, Projectile.height, DustID.AncientLight, velocity.X, velocity.Y);
+                dust.noGravity = true;
+            }
+        }
+        private void Animate()
+        {
+            //Advance frames every third tick
+            if (Main.GameUpdateCount % 3 != 0)
+                return;
+
+            Projectile.frame++;
+            switch (Projectile.frame)
+            {
+                case 4:
+                    animate = false;
+                    Projectile.netUpdate = true;
+                    break;
+                case 11:
+                    Projectile.Kill();
+                    break;
+            }
+        }
+    }
+
+    /// <summary>
+    /// A large projectile which flies in a straight path, and explodes into smaller projectiles that home toward the caster
+    /// </summary>
+    public class Godcat_LightReturnBall : ModProjectile
+    {
+        private bool IsMiniVariant => Projectile.ai[1] == 1;
+        private NPC Owner => Main.npc[(int)Projectile.ai[0]]; //Projectile.owner must be -1, otherwise it doesn't deal damage to the player. So we will pass the owner through ai
+        public override void SetDefaults()
+        {
+            Projectile.width = 20;
+            Projectile.height = 20;
+            Projectile.timeLeft = 80;
+            Projectile.hostile = true;
+            Projectile.friendly = false;
+            Projectile.tileCollide = false;
+        }
+        public override bool? CanHitNPC(NPC target) => false;
+        public override void OnSpawn(IEntitySource source)
+        {
+            if (IsMiniVariant)
+            {
+                Projectile.scale = 0.8f;
+                Projectile.timeLeft = 200;
+            }
+            else
+            {
+                Projectile.scale = 1.2f;
+            }
+        }
+        public override void AI()
+        {
+            if (IsMiniVariant)
+            {
+                if (Main.rand.NextBool(2))
+                {
+                    var dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.AncientLight);
+                    dust.noGravity = true;
+                }
+
+                Projectile.HomeTowards(Owner, 0.3f);
+                if(Projectile.Distance(Owner.Center) < 32)
+                {
+                    Projectile.Kill();
+                }
+            }
+            else
+            {
+                var dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.AncientLight);
+                dust.noGravity = true;
+            }
+        }
+        public override void OnKill(int timeLeft)
+        {
+            if (!IsMiniVariant)
+            {
+                //Replace projectile with a bunch of mini variants, with a bit of angle variation
+                var amount = 8;
+                var spread = 1f;
+                for (float theta = -spread; theta < spread; theta += 2 * spread / amount)
+                {
+                    var velocity = Projectile.velocity.RotatedBy(theta) * Main.rand.NextFloat(0.9f, 1.1f);
+                    Projectile.NewProjectile(Projectile.GetSource_Death(), Projectile.position, velocity, Projectile.type, Projectile.damage, Projectile.knockBack, Projectile.owner, Owner.whoAmI, 1);
+                }
+            }
+
+            //Create dust
+            for (int i = 0; i < 20; i++)
+            {
+                Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.AncientLight);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Basic ball projectile that flies forward
+    /// </summary>
+    public class Godcat_LightBall : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 16;
+            Projectile.height = 16;
+            Projectile.timeLeft = 360;
+            Projectile.hostile = true;
+            Projectile.friendly = false;
+            Projectile.tileCollide = false;
+        }
+        public override bool? CanHitNPC(NPC target) => false;
+        public override void AI()
+        {
+            if (Main.rand.NextBool(2))
+            {
+                var dust = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, DustID.AncientLight);
+                dust.noGravity = true;
+            }
+        }
+        public override void OnKill(int timeLeft)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.AncientLight);
+            }
+        }
     }
 }
