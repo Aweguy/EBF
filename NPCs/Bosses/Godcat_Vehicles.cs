ac898b39a4f7ac61860ea20bd3d24b15b2273617
RasmusBayThomsen <mikemocals@gmail.com>
2025-07-06 10:53:09 +0200

    Added the godcat vehicles

---
+++ NPCs/Bosses/Godcat_Vehicles.cs
@@ -0,0 +1,189 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Bestiary;
+
+namespace EBF.NPCs.Bosses
+{
+    public abstract class Godcat_Vehicle : ModNPC
+    {
+        //Textures
+        protected Texture2D currentTexture;
+        protected Asset<Texture2D> idleTexture;
+        protected Asset<Texture2D> attackTexture;
+
+        //AI
+        private enum State : byte { Idle }
+        private State currentState = State.Idle;
+        private readonly Dictionary<State, int> stateDurations = new()
+        {
+            [State.Idle] = 200,
+        };
+        private ref float StateTimer => ref NPC.localAI[0];
+
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[Type] = 8;
+            NPCID.Sets.DontDoHardmodeScaling[Type] = true;
+            NPCID.Sets.SpecificDebuffImmunity[Type][BuffID.Poisoned] = true;
+            NPCID.Sets.SpecificDebuffImmunity[Type][BuffID.Confused] = true;
+        }
+        public override void SetDefaults()
+        {
+            NPC.width = 152;
+            NPC.height = 152;
+            NPC.damage = 100;
+            NPC.defense = 50;
+            NPC.lifeMax = 200000;
+            NPC.noGravity = true;
+
+            NPC.noTileCollide = true;
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath1;
+            NPC.knockBackResist = 0f;
+            NPC.value = Item.buyPrice(gold: 5);
+            NPC.SpawnWithHigherTime(30);
+            NPC.boss = true;
+            NPC.npcSlots = 15f; // Use all spawn slots to prevent random NPCs from spawning
+
+            NPC.lavaImmune = true;
+
+            Music = MusicLoader.GetMusicSlot(Mod, "Assets/Music/Fallen_Blood");
+        }
+        public override bool CanHitPlayer(Player target, ref int cooldownSlot)
+        {
+            cooldownSlot = ImmunityCooldownID.Bosses; //Prevent ignoring boss attacks by taking damage from other sources.
+            return true;
+        }
+        public override void BossLoot(ref string name, ref int potionType)
+        {
+            potionType = ItemID.SuperHealingPotion;
+        }
+        public override void FindFrame(int frameHeight)
+        {
+            // Idle frames
+            NPC.frameCounter += 0.1f;
+            if (NPC.frameCounter >= Main.npcFrameCount[NPC.type])
+            {
+                NPC.frameCounter = 0;
+            }
+            NPC.frame.Y = (int)NPC.frameCounter * frameHeight;
+        }
+        public override void AI()
+        {
+            NPC.TargetClosest();
+            NPC.spriteDirection = NPC.direction;
+            Player player = Main.player[NPC.target];
+
+            if (player.dead)
+            {
+                NPC.EncourageDespawn(10); // Despawns in 10 ticks
+                return;
+            }
+
+            Move(player);
+            HandleStateChanging();
+        }
+        public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+        {
+            if (!idleTexture.IsLoaded)
+                return false;
+
+            var position = NPC.Center - screenPos;
+            var origin = NPC.Size * 0.5f;
+            var flipX = NPC.direction == 1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+            spriteBatch.Draw(currentTexture, position, NPC.frame, drawColor, 0, origin, 1, flipX, 0);
+            return false;
+        }
+        protected abstract void Move(Player player);
+        private void HandleStateChanging()
+        {
+            StateTimer++;
+            if (StateTimer >= stateDurations[currentState])
+            {
+                StateTimer = 0;
+                currentState = currentState != State.Idle ? (State)Main.rand.Next(1, stateDurations.Count + 1) : State.Idle;
+            }
+        }
+    }
+
+    [AutoloadBossHead]
+    public class Godcat_Creator : Godcat_Vehicle
+    {
+        public override string Texture => "EBF/NPCs/Bosses/Godcat_Destroyer";
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+
+            //Bestiary
+            NPCID.Sets.BossBestiaryPriority.Add(Type); //Grouped with other bosses
+            var drawModifiers = new NPCID.Sets.NPCBestiaryDrawModifiers()
+            {
+                CustomTexturePath = "EBF/Assets/Textures/Bestiary/Godcat_Preview",
+                PortraitScale = 0.6f, // Portrait refers to the full picture when clicking on the icon in the bestiary
+                PortraitPositionYOverride = 0f,
+            };
+            NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, drawModifiers);
+        }
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            idleTexture = ModContent.Request<Texture2D>(Texture);
+            currentTexture = idleTexture.Value;
+        }
+        public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+        {
+            bestiaryEntry.Info.AddRange([
+                new MoonLordPortraitBackgroundProviderBestiaryInfoElement(), // Plain black background
+				new FlavorTextBestiaryInfoElement("Mods.EBF.Bestiary.Godcat_Creator")
+            ]);
+        }
+        protected override void Move(Player player)
+        {
+            var preferredPosition = player.Center + new Vector2(550, -100);
+            NPC.Center = Vector2.Lerp(NPC.Center, preferredPosition, 0.03f);
+        }
+    }
+
+    [AutoloadBossHead]
+    public class Godcat_Destroyer : Godcat_Vehicle
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+
+            //Bestiary
+            NPCID.Sets.BossBestiaryPriority.Add(Type); //Grouped with other bosses
+            var drawModifiers = new NPCID.Sets.NPCBestiaryDrawModifiers()
+            {
+                CustomTexturePath = "EBF/Assets/Textures/Bestiary/Godcat_Preview",
+                PortraitScale = 0.6f, // Portrait refers to the full picture when clicking on the icon in the bestiary
+                PortraitPositionYOverride = 0f,
+            };
+            NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, drawModifiers);
+        }
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+            idleTexture = ModContent.Request<Texture2D>(Texture);
+            attackTexture = ModContent.Request<Texture2D>(Texture + "_Attack");
+            currentTexture = idleTexture.Value;
+        }
+        public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+        {
+            bestiaryEntry.Info.AddRange([
+                new MoonLordPortraitBackgroundProviderBestiaryInfoElement(), // Plain black background
+				new FlavorTextBestiaryInfoElement("Mods.EBF.Bestiary.Godcat_Destroyer")
+            ]);
+        }
+        protected override void Move(Player player)
+        {
+            var preferredPosition = player.Center + new Vector2(-550, -100);
+            NPC.Center = Vector2.Lerp(NPC.Center, preferredPosition, 0.05f);
+        }
+    }
+}
