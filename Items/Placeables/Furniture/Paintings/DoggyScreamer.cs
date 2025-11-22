156b745d5192568c17fd6c1e71c104793d619f91
TheDigitalZero <mikemocals@gmail.com>
2025-11-22 14:01:49 +0100

    Added 7 new paintings

---
+++ Items/Placeables/Furniture/Paintings/DoggyScreamer.cs
@@ -0,0 +1,28 @@
+﻿using EBF.Tiles.Furniture.Paintings;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace EBF.Items.Placeables.Furniture.Paintings
+{
+    public class DoggyScreamer : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Placeables.Furniture.Paintings";
+
+        public override void SetDefaults()
+        {
+            Item.width = 96;
+            Item.height = 64;
+            Item.maxStack = 9999;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
+            Item.value = Item.buyPrice(0, 2, 0, 0);
+            Item.rare = ItemRarityID.White;
+            Item.createTile = ModContent.TileType<DoggyScreamerTile>();
+        }
+    }
+}
---
+++ Tiles/Furniture/Paintings/HeavyImpracticalBowsTile.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace EBF.Tiles.Furniture.Paintings
+{
+    public class HeavyImpracticalBowsTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            Main.tileSpelunker[Type] = true;
+            Main.tileWaterDeath[Type] = false;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3Wall);
+            TileObjectData.newTile.Width = 6;
+            TileObjectData.newTile.Height = 4;
+            TileObjectData.newTile.Origin = new Point16(2, 2);
+            TileObjectData.newTile.CoordinateHeights = [16, 16, 16, 16];
+            TileObjectData.newTile.LavaDeath = true;
+            TileObjectData.addTile(Type);
+
+            TileID.Sets.DisableSmartCursor[Type] = true;
+            TileID.Sets.FramesOnKillWall[Type] = true;
+            TileID.Sets.DisableSmartCursor[Type] = true;
+            DustType = DustID.WoodFurniture;
+            AddMapEntry(new Color(99, 50, 30), Language.GetText("MapObject.Painting"));
+        }
+    }
+}
