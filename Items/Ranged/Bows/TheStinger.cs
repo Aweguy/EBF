a29b27d47d1c98256250952ed73c1006ce206a6c
digitalzero <mikemocals@gmail.com>
2026-06-06 11:05:17 +0200

    Migrated TheStinger.cs to new bow system.

--- Items/Ranged/Bows/TheStinger.cs
+++ Items/Ranged/Bows/TheStinger.cs
@@ -1,14 +1,16 @@
 ﻿using EBF.Abstract_Classes;
 using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace EBF.Items.Ranged.Bows
 {
-    public class TheStinger : ModItem, ILocalizedModType
+    public class TheStinger : EBFBow, ILocalizedModType
     {
         public new string LocalizationCategory => "Items.Weapons.Ranged.Bows";
+        protected override int HoldoutProjectile => ModContent.ProjectileType<TheStinger_HoldoutProjectile>();
         public override void SetDefaults()
         {
             Item.width = 26;//Width of the hitbox of the item (usually the item's sprite width)
@@ -16,30 +18,13 @@ namespace EBF.Items.Ranged.Bows
 
             Item.damage = 31;//Item's base damage value
             Item.knockBack = 3;//Float, the item's knockback value. How far the enemy is launched when hit
-            Item.DamageType = DamageClass.Ranged;//Item's damage type, Melee, Ranged, Magic and Summon. Custom damage are also a thing
-            Item.useStyle = ItemUseStyleID.Shoot;//The animation of the item when used
             Item.useTime = 8;//How fast the item is used
             Item.useAnimation = 8;//How long the animation lasts. For swords it should stay the same as UseTime
 
             Item.value = Item.sellPrice(copper: 0, silver: 90, gold: 8, platinum: 0);//Item's value when sold
             Item.rare = ItemRarityID.Pink;//Item's name colour, this is hardcoded by the modder and should be based on progression
-            Item.UseSound = SoundID.Item32;//The item's sound when it's used
-            Item.autoReuse = true;//Boolean, if the item auto reuses if the use button is held
-            Item.useTurn = false;//Boolean, if the player's direction can change while using the item
-
-            Item.useAmmo = AmmoID.Arrow;
-            Item.shoot = ProjectileID.WoodenArrowFriendly;
             Item.shootSpeed = 8f;
-            Item.channel = true;
-            Item.noMelee = true;
-        }
-        public override bool CanUseItem(Player player) => player.HasAmmo(player.HeldItem) && !player.noItems && !player.CCed;
-        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
-        {
-            if (type == ProjectileID.WoodenArrowFriendly)
-            {
-                type = ModContent.ProjectileType<TheStinger_Arrow>();
-            }
+            base.SetDefaults();
         }
         public override void AddRecipes()
         {
@@ -52,40 +37,34 @@ namespace EBF.Items.Ranged.Bows
         }
     }
 
-    public class TheStinger_Arrow : EBFChargeableArrow
+    public class TheStinger_HoldoutProjectile : EBFHoldoutBow
     {
-        public override string Texture => $"Terraria/Images/Projectile_{ProjectileID.WoodenArrowFriendly}";
+        protected override int ArrowType => ProjectileID.WoodenArrowFriendly;
+        public override string Texture => "EBF/Items/Ranged/Bows/TheStinger";
         public override void SetDefaults()
         {
-            Projectile.width = 10;
-            Projectile.height = 10;
-
-            Projectile.friendly = false;
-            Projectile.tileCollide = true;
-            Projectile.hide = false;
-            Projectile.DamageType = DamageClass.Ranged;
-            Projectile.aiStyle = ProjAIStyleID.Arrow;
-            Projectile.ignoreWater = true;
-
+            Projectile.width = 26;
+            Projectile.height = 66;
             MaximumDrawTime = 10;
             DamageScale = 1.0f;
             VelocityScale = 1.33f;
-
-            Projectile.localNPCHitCooldown = -1;
-            Projectile.usesLocalNPCImmunity = true;
+            base.SetDefaults();
         }
 
-        public override void OnProjectileRelease()
+        protected override void OnShoot(IEntitySource source, Vector2 position, Vector2 velocity, int type, int damage, float knockback, int owner,
+            float ai0 = 0, float ai1 = 0, float ai2 = 0)
         {
             if (FullyCharged)
             {
                 Projectile.Kill();
-                for (int i = 0; i < 3; i++)
+                for (var i = 0; i < 3; i++)
                 {
-                    Projectile proj = Projectile.NewProjectileDirect(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.velocity.RotatedByRandom(0.2d), ProjectileID.HornetStinger, Projectile.damage, Projectile.knockBack, Projectile.owner);
+                    var proj = Projectile.NewProjectileDirect(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.velocity.RotatedByRandom(0.2d), ProjectileID.HornetStinger, Projectile.damage, Projectile.knockBack, Projectile.owner);
                     proj.DamageType = DamageClass.Ranged;
                 }
             }
+            else
+                base.OnShoot(source, position, velocity, type, damage, knockback, owner, ai0, ai1, ai2);
         }
     }
 }
