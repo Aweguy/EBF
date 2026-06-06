b74e1fb6d74efbc25b809e41e7e52193ea40f063
digitalzero <mikemocals@gmail.com>
2026-06-06 11:04:59 +0200

    Migrated Sharanga.cs to new bow system.

--- Items/Ranged/Bows/Sharanga.cs
+++ Items/Ranged/Bows/Sharanga.cs
@@ -6,9 +6,10 @@ using Terraria.ModLoader;
 
 namespace EBF.Items.Ranged.Bows
 {
-    public class Sharanga : ModItem, ILocalizedModType
+    public class Sharanga : EBFBow, ILocalizedModType
     {
         public new string LocalizationCategory => "Items.Weapons.Ranged.Bows";
+        protected override int HoldoutProjectile => ModContent.ProjectileType<Sharanga_HoldoutProjectile>();
         public override void SetDefaults()
         {
             Item.width = 20;//Width of the hitbox of the item (usually the item's sprite width)
@@ -16,33 +17,13 @@ namespace EBF.Items.Ranged.Bows
 
             Item.damage = 78;//Item's base damage value
             Item.knockBack = 3;//Float, the item's knockback value. How far the enemy is launched when hit
-            Item.DamageType = DamageClass.Ranged;//Item's damage type, Melee, Ranged, Magic and Summon. Custom damage are also a thing
-            Item.useStyle = ItemUseStyleID.Shoot;//The animation of the item when used
             Item.useTime = 30;//How fast the item is used
             Item.useAnimation = 30;//How long the animation lasts. For swords it should stay the same as UseTime
 
             Item.value = Item.sellPrice(copper: 0, silver: 20, gold: 10, platinum: 0);//Item's value when sold
             Item.rare = ItemRarityID.Cyan;//Item's name colour, this is hardcoded by the modder and should be based on progression
-            Item.UseSound = SoundID.Item32;//The item's sound when it's used
-            Item.autoReuse = true;//Boolean, if the item auto reuses if the use button is held
-            Item.useTurn = false;//Boolean, if the player's direction can change while using the item
-
-            Item.useAmmo = AmmoID.Arrow;
-            Item.shoot = ProjectileID.WoodenArrowFriendly;
             Item.shootSpeed = 10f;
-            Item.channel = true;
-            Item.noMelee = true;
-        }
-        public override bool CanUseItem(Player player)
-        {
-            return player.HasAmmo(player.HeldItem) && !player.noItems && !player.CCed;
-        }
-        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
-        {
-            if (type == ProjectileID.WoodenArrowFriendly)
-            {
-                type = ModContent.ProjectileType<Sharanga_Arrow>();
-            }
+            base.SetDefaults();
         }
         public override void AddRecipes()
         {
@@ -55,7 +36,22 @@ namespace EBF.Items.Ranged.Bows
         }
     }
 
-    public class Sharanga_Arrow : EBFChargeableArrow
+    public class Sharanga_HoldoutProjectile : EBFHoldoutBow
+    {
+        protected override int ArrowType => ModContent.ProjectileType<Sharanga_Arrow>();
+        public override string Texture => "EBF/Items/Ranged/Bows/Sharanga";
+        public override void SetDefaults()
+        {
+            Projectile.width = 20;
+            Projectile.height = 70;
+            MaximumDrawTime = 100;
+            DamageScale = 3f;
+            VelocityScale = 2f;
+            base.SetDefaults();
+        }
+    }
+
+    public class Sharanga_Arrow : ModProjectile
     {
         private int bounces = 2;
         public override void SetDefaults()
@@ -66,17 +62,10 @@ namespace EBF.Items.Ranged.Bows
             Projectile.extraUpdates = 1; //Don't forget that extra updates also increases perceived velocity
             Projectile.penetrate = -1;
 
-            Projectile.friendly = false;
-            Projectile.tileCollide = true;
-            Projectile.hide = false;
+            Projectile.friendly = true;
             Projectile.DamageType = DamageClass.Ranged;
             Projectile.aiStyle = ProjAIStyleID.Arrow;
             Projectile.ignoreWater = true;
-
-            MaximumDrawTime = 100;
-            DamageScale = 3f;
-            VelocityScale = 2f;
-
             Projectile.localNPCHitCooldown = -1;
             Projectile.usesLocalNPCImmunity = true;
         }
@@ -84,9 +73,9 @@ namespace EBF.Items.Ranged.Bows
         {
             //Trail
             Lighting.AddLight(Projectile.Center, TorchID.Blue);
-            for (int i = 0; i < 5; i++)
+            for (var i = 0; i < 5; i++)
             {
-                Dust dust = Dust.NewDustPerfect(Projectile.Center, DustID.BlueTorch, Vector2.Zero, Scale: 1f);
+                var dust = Dust.NewDustPerfect(Projectile.Center, DustID.BlueTorch, Vector2.Zero, Scale: 1f);
                 dust.position -= Projectile.velocity / 5f * i;
                 dust.noGravity = true;
             }
